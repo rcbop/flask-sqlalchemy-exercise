83eda13415cb8072a66138b60388fbfea2fcd86f
Rogério Peixoto <rcbpeixoto@gmail.com>
2023-02-17 09:24:03 +0000

    dry tests

--- api/app.py
+++ api/app.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask
-from flask_smorest import Api
-from api.db import db
 
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_smorest import Api
+
+from api.db import db
 from api.resources.item import blp as ItemBlueprint
 from api.resources.store import blp as StoreBlueprint
 from api.resources.tag import blp as TagBlueprint
 
-def create_app(db_url: str | None = None) -> Api:
+
+def create_app(db_url: str | None = None, jwt_secret: str | None = None) -> Api:
     app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -17,14 +20,19 @@ def create_app(db_url: str | None = None) -> Api:
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db")
+    app.config["JWT_SECRET_KEY"] = jwt_secret or os.getenv(
+        "JWT_SECRET_KEY", "super-secret")
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
 
     api = Api(app)
+
+    jwt = JWTManager(app)
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
--- test/test_app.py
+++ test/test_app.py
@@ -2,19 +2,13 @@ from api.app import create_app
 
 def test_create_app():
     app = create_app()
-    assert app is not None
-    assert app.config["PROPAGATE_EXCEPTIONS"] is True
-    assert app.config["API_TITLE"] == "Stores REST API"
-    assert app.config["API_VERSION"] == "v1"
-    assert app.config["OPENAPI_VERSION"] == "3.0.3"
-    assert app.config["OPENAPI_URL_PREFIX"] == "/"
-    assert app.config["OPENAPI_SWAGGER_UI_PATH"] == "/swagger-ui"
-    assert app.config["OPENAPI_SWAGGER_UI_URL"] == "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    assert app.config["SQLALCHEMY_DATABASE_URI"] == "sqlite:///data.db"
-    # assert app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] is False
+    assert_configs(app)
 
 def test_create_app_with_db_url():
     app = create_app("sqlite:///test.db")
+    assert_configs(app, "sqlite:///test.db")
+
+def assert_configs(app, db_url="sqlite:///data.db"):
     assert app is not None
     assert app.config["PROPAGATE_EXCEPTIONS"] is True
     assert app.config["API_TITLE"] == "Stores REST API"
@@ -23,4 +17,5 @@ def test_create_app_with_db_url():
     assert app.config["OPENAPI_URL_PREFIX"] == "/"
     assert app.config["OPENAPI_SWAGGER_UI_PATH"] == "/swagger-ui"
     assert app.config["OPENAPI_SWAGGER_UI_URL"] == "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    assert app.config["SQLALCHEMY_DATABASE_URI"] == "sqlite:///test.db"
+    assert app.config["SQLALCHEMY_DATABASE_URI"] == db_url
+    assert app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] is False
